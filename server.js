77abf1e85299ccdcfb79040ba0695a49f4d608c8
xzxhub0-source <xzxhub0@gmail.com>
2026-01-31 22:28:10 -0500

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from "http";
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Bot is alive");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("XZX bot alive");
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("🌐 Keep-alive server running");
+server.listen(PORT, () => {
+  console.log(`🌐 Keep-alive server running on port ${PORT}`);
 });
